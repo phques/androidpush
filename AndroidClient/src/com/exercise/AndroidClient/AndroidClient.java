5cc3571f7223e5ad46327f56e550ba44a8ce8f9c
kwez <kwez@aa81b46f-3ad1-43f9-aa80-f651a276bc9b>
2012-07-30 00:55:46 +0000

    wip, receive a "recvFrom" command in the datagram using JSON

--- AndroidClient/src/com/exercise/AndroidClient/AndroidClient.java
+++ AndroidClient/src/com/exercise/AndroidClient/AndroidClient.java
@@ -1,12 +1,17 @@
 package com.exercise.AndroidClient;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -22,7 +27,30 @@ public class AndroidClient extends Activity {
 	EditText textOut;
 	TextView textIn;
 	DownloadWebpageText downloader = null;
+	DatagramSocket socket = null;
+	int cptThread = 0;
 
+	class RecvFrom {
+
+		int port;
+		String destinationType;  
+		String subDir;  
+		String file;  
+		
+		RecvFrom(String json) throws JSONException {
+			parseJson(json);
+		}
+		
+		private void parseJson(String json) throws JSONException {			
+			JSONObject object = (JSONObject) new JSONTokener(json).nextValue();
+			port = object.getInt("recvFromPort");
+			file = object.getString("file");  
+			subDir = object.getString("subDir");  
+			destinationType = object.getString("destinationType");  
+		}
+		
+	}
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,59 +58,104 @@ public class AndroidClient extends Activity {
 		setContentView(R.layout.main);
 
 		textOut = (EditText)findViewById(R.id.textout);
-		Button buttonSend = (Button)findViewById(R.id.send);
 		textIn = (TextView)findViewById(R.id.textin);
+		
+		Button buttonSend = (Button)findViewById(R.id.send);
 		buttonSend.setOnClickListener(buttonSendOnClickListener);
 	}
 
-	Button.OnClickListener buttonSendOnClickListener
-	= new Button.OnClickListener(){
 
-		//		@Override
+	Button.OnClickListener buttonSendOnClickListener =
+			new Button.OnClickListener() {
 		public void onClick(View arg0) {
-			textOut.setText("waiting ...");
-			new DownloadWebpageText().execute();
-//			downloader = new DownloadWebpageText(); 
-//			downloader.execute();
+			if (socket != null) {
+				if (downloader == null) {
+					textOut.setText("waiting ...");
+					//						new DownloadWebpageText().execute();
+					downloader = new DownloadWebpageText(); 
+					downloader.execute();
+				}
+				else {
+					cptThread++;
+					textOut.setText("already waiting ... " + cptThread);
+				}
+			}
+			else {
+				textOut.setText("no socket !!");
+			}
 		}};
+	
+	@Override
+	public void onStart() {
+		super.onStart();  // Always call the superclass method first
+
+		try {
+			socket = new DatagramSocket(null);	// unbound..
+			socket.setReuseAddress(true);		// .. and reuse ! (so we can restart while debugging)
+			socket.bind(new InetSocketAddress(4445)); // void return ! cant check if worked !!!
+		}
+		catch (SocketException e) {
+			e.printStackTrace();
+			textOut.setText(e.getMessage());
+			
+			if (socket != null){
+				socket.close();
+				socket = null;
+			}
+		}		
+	}
+	
+	
+	@Override
+	public void onStop() {
+		super.onStop();  // Always call the superclass method first
+		
+		if (socket != null) {
+			socket.close();
+			socket = null;
+		}
+	}
+	
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();  // Always call the superclass
+	    
+	    // probably not needed, onStop closes the socket, which should make the thread stop (?)
+	    if (downloader != null)
+	    	downloader.cancel(true);
+	    
+	    downloader = null;
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	/*-----------------------------------------*/
 	
-//	public void onStop() {
-//		super.onStop();  // Always call the superclass method first
-//		if (downloader != null)
-//			downloader.cancel(true);
-//		downloader = null;
-//	}
+	
+	private class DownloadWebpageText extends AsyncTask<Void, Void, String> {
 
-	private class DownloadWebpageText extends AsyncTask<Void, Void, Long> {
-		private String result;
 		//@Override
-		protected Long doInBackground(Void ... voids) {
+		protected String doInBackground(Void ... voids) {
 
+			String result=null;
 //			Socket socket = null;
-			BufferedReader in = null;
-			DatagramSocket socket = null;
+//			BufferedReader in = null;
 			
 			try {
-//				socket = new DatagramSocket(4445);
-				socket = new DatagramSocket(null);	// unbound..
-				socket.setReuseAddress(true);		// .. and reuse ! (so we can restart while debugging)
-				socket.bind(new InetSocketAddress(4445)); // void return ! cant check if worked !!!
-//				socket.setBroadcast(true);
-
 				byte[] buf = new byte[1024*4];
 				DatagramPacket packet = new DatagramPacket(buf, buf.length);
 				socket.receive(packet);
 
-				result = new String(packet.getData(), 0, packet.getLength());
+				String json = new String(packet.getData(), 0, packet.getLength());
+				RecvFrom recvFrom = new RecvFrom(json);
+				
 				InetAddress addr = packet.getAddress();
-				result += addr.toString();
-								
+				result = "received " + json + "\nfrom : " + addr.toString();
+				
 				/*
 				socket = new Socket();
 				socket.connect(new InetSocketAddress("192.168.1.119", 8888), 0);
@@ -90,47 +163,50 @@ public class AndroidClient extends Activity {
 				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));				
 				result = in.readLine();
 				*/
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
+			/*} catch (UnknownHostException e) {
 				e.printStackTrace();
 				result = e.getMessage();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			*/} catch (IOException e) {
 				e.printStackTrace();
 				result = e.getMessage();
-			} catch (Exception e) {
+			} /*catch (Exception e) {
+				e.printStackTrace();
+				result = e.getMessage();
+			}*/ catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				result = e.getMessage();
 			}
-			finally{
+			/*finally{
 				if (socket != null){
 					socket.close();
-//					try {
-//						socket.close();
-//					} catch (IOException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
+
+					try {
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 				
 				if (in != null){
 					try {
 						in.close();
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
-			}
-			return 0L;
+			}*/
+			return result;
 		}
 		
+
 		// onPostExecute displays the results of the AsyncTask.
 		//@Override
-		protected void onPostExecute(Long tt) {
-			textOut.setText(result);
-//			downloader = null;
+		protected void onPostExecute(String result) {
+			if (result != null)
+				textOut.setText(result);
+			
+			downloader = null;
 		}
 	}	    
 
