88bf44bab419bf9f1caf4b159ef07427e600b205
kwez <kwez@aa81b46f-3ad1-43f9-aa80-f651a276bc9b>
2012-07-30 00:52:50 +0000

    wip

--- fileServerC/main.c
+++ fileServerC/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,12 +17,11 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
     FILE* file = 0;
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[1024*16+1];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     size_t filen, total=0;
+    int sockfd, newsockfd, portno, n;
+    struct sockaddr_in serv_addr, cli_addr;
+    size_t filen, total=0;
+    socklen_t clilen;
+    char buffer[1024*16+1];
     char* filename = 0;
 
     if (argc < 2)
@@ -49,41 +47,43 @@ int main(int argc, char *argv[])
     else
         portno = 8888;
 
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
 
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd,
-                 (struct sockaddr *) &cli_addr,
-                 &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd,
+                       (struct sockaddr *) &cli_addr,
+                       &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
 
-    do {
+    do
+    {
 
-     bzero(buffer,1024*16);
-     filen=fread(buffer, 1, 1024*16, file);
-     n = write(newsockfd, buffer, filen);
-     if (n < 0) error("ERROR writing to socket");
-     if (filen < 1024*16) printf("\nfilen %d\n", filen);
-     total += filen;
-     printf("%d\r", total);
-    } while (filen > 0);
+        bzero(buffer,1024*16);
+        filen=fread(buffer, 1, 1024*16, file);
+        n = write(newsockfd, buffer, filen);
+        if (n < 0) error("ERROR writing to socket");
+        if (filen < 1024*16) printf("\nfilen %d\n", filen);
+        total += filen;
+        printf("%d\r", total);
+    }
+    while (filen > 0);
 
-     close(newsockfd);
-     close(sockfd);
+    close(newsockfd);
+    close(sockfd);
     fclose(file);
 
     puts("");
 
-	return 0;
+    return 0;
 }
