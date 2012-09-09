294530f23add3b3544c1e7f202fd7fd066b91e3f
Philippe Quesnel <philippe.quesnel@gmail.com>
2012-09-09 03:18:20 -0400

    fltk gui. 1st main window version

--- gui-fltk/mainWndFl.cxx
+++ gui-fltk/mainWndFl.cxx
@@ -6,13 +6,14 @@
 #include "mainWndFl.h"
 
 MainWindowFl::MainWindowFl() {
-  { window = new Fl_Double_Window(260, 375);
+  { window = new Fl_Double_Window(255, 375, "Android Push");
     window->user_data((void*)(this));
     window->align(Fl_Align(FL_ALIGN_CLIP|FL_ALIGN_INSIDE));
     { Fl_Group* o = new Fl_Group(15, 25, 224, 110, "Destination");
       o->box(FL_ENGRAVED_FRAME);
       o->labeltype(FL_EMBOSSED_LABEL);
       { Fl_Box* o = new Fl_Box(20, 31, 55, 19, "Root");
+        o->labelfont(2);
         o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
       } // Fl_Box* o
       { rootDestChoice = new Fl_Choice(24, 49, 207, 25);
@@ -20,6 +21,7 @@ MainWindowFl::MainWindowFl() {
         rootDestChoice->labeltype(FL_NO_LABEL);
       } // Fl_Choice* rootDestChoice
       { Fl_Box* o = new Fl_Box(20, 78, 75, 19, "Subdir");
+        o->labelfont(2);
         o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
       } // Fl_Box* o
       { subdirInput = new Fl_Input(24, 98, 207, 25);
@@ -33,12 +35,14 @@ MainWindowFl::MainWindowFl() {
     { Fl_Group* o = new Fl_Group(15, 159, 224, 172, "Local");
       o->box(FL_ENGRAVED_FRAME);
       o->labeltype(FL_EMBOSSED_LABEL);
-      { Fl_Box* o = new Fl_Box(20, 169, 54, 19, "Root");
+      { Fl_Box* o = new Fl_Box(20, 167, 54, 19, "Root");
+        o->labelfont(2);
         o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
       } // Fl_Box* o
-      { localRootInput = new Fl_Input(24, 189, 207, 25);
+      { localRootInput = new Fl_Input(24, 187, 207, 25);
       } // Fl_Input* localRootInput
       { Fl_Box* o = new Fl_Box(20, 219, 54, 19, "Files");
+        o->labelfont(2);
         o->align(Fl_Align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE));
       } // Fl_Box* o
       { filesBrowser = new Fl_Browser(24, 239, 207, 85);
@@ -50,8 +54,19 @@ MainWindowFl::MainWindowFl() {
       o->end();
       Fl_Group::current()->resizable(o);
     } // Fl_Group* o
-    { new Fl_Button(178, 340, 60, 25, "Push");
-    } // Fl_Button* o
+    { Fl_Group* o = new Fl_Group(10, 339, 238, 29);
+      { Fl_Button* o = new Fl_Button(145, 339, 96, 28, "Push  @-2<-");
+        o->labeltype(FL_EMBOSSED_LABEL);
+        o->labelfont(3);
+        o->labelsize(15);
+        o->labelcolor((Fl_Color)27);
+      } // Fl_Button* o
+      { Fl_Box* o = new Fl_Box(50, 345, 50, 15, "foresize");
+        o->hide();
+        Fl_Group::current()->resizable(o);
+      } // Fl_Box* o
+      o->end();
+    } // Fl_Group* o
     window->size_range(window->w(), window->h());
     window->end();
   } // Fl_Double_Window* window
