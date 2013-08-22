96e274fa93962c59a69e4b60e44ea377dcdec47d
Philippe Quesnel <philippe.quesnel@gmail.com>
2013-08-21 22:23:02 -0400

    working on mindmaps, androidPush & marcoPolo, added marcoPoloSimple, braught the idea back to basics

---
+++ marcoPolo/marco polo simple.mm
@@ -0,0 +1,77 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1377099294490" ID="ID_394723472" MODIFIED="1377132702936" STYLE="fork" TEXT="marco polo&#xa;simple">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1377100373355" FOLDED="true" HGAP="16" ID="ID_959423427" MODIFIED="1377133913210" POSITION="right" TEXT="marcoPolo / app connect" VSHIFT="-24">
+<node CREATED="1377099309997" FOLDED="true" HGAP="23" ID="ID_1115893101" MODIFIED="1377133881874" STYLE="fork" TEXT="waits on UDP port for connections / requests" VSHIFT="-24">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1377099957835" HGAP="26" ID="ID_781887355" MODIFIED="1377100410620" TEXT="could use range/list of ports&#xa;in case of failure to open port" VSHIFT="10">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1377100089551" FOLDED="true" ID="ID_1921755573" MODIFIED="1377133912246" TEXT="app &apos;connects&apos; to marcoPolo (UDP broadcast)">
+<node CREATED="1377101357557" ID="ID_175850506" MODIFIED="1377101387636" TEXT="marcoPolo sends back &quot;your appId&quot;"/>
+<node CREATED="1377100161221" FOLDED="true" ID="ID_1622029243" MODIFIED="1377133911554" TEXT="app then connects to &#xa;a fixed TCP socket" VSHIFT="16">
+<node CREATED="1377100482850" HGAP="37" ID="ID_1208356818" MODIFIED="1377133906000" TEXT="marcoPol sends PingAlive" VSHIFT="7"/>
+<node CREATED="1377100725872" HGAP="40" ID="ID_535245107" MODIFIED="1377132401749" TEXT="unregister app when no answer" VSHIFT="11"/>
+</node>
+</node>
+<node CREATED="1377101264226" FOLDED="true" ID="ID_852292473" MODIFIED="1377133802816" TEXT="When app closes:&#xa;&quot;unregister me&quot; (appId)">
+<node CREATED="1377101324067" HGAP="25" ID="ID_924797808" MODIFIED="1377131047094" TEXT="could be done by just closing permanent TCP socket" VSHIFT="12">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1377099674415" FOLDED="true" HGAP="34" ID="ID_558032627" MODIFIED="1377133809750" POSITION="right" STYLE="fork" TEXT="msgs discovery" VSHIFT="4">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1377101566981" ID="ID_58866484" MODIFIED="1377132274128" TEXT="appY registers msg&#xa;&apos;kwez.org/androidPush/getPush&apos;"/>
+<node CREATED="1377099684469" HGAP="24" ID="ID_448274881" MODIFIED="1377132292065" STYLE="fork" TEXT="appX queries for msg&#xa;&apos;kwez.org/androidPush/getPush&apos;" VSHIFT="17">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1377101959898" FOLDED="true" ID="ID_1955642783" MODIFIED="1377133808766" TEXT="marcoPolo answers appX with&#xa;host:port">
+<node CREATED="1377102046425" ID="ID_1692447688" MODIFIED="1377132351451" TEXT="appX talks to appY ..."/>
+<node CREATED="1377132538773" ID="ID_877205127" MODIFIED="1377132569163" TEXT="protocol for this msg must&#xa;be decided between apps"/>
+</node>
+</node>
+<node CREATED="1377132575487" FOLDED="true" HGAP="38" ID="ID_778880487" MODIFIED="1377133811630" POSITION="right" TEXT="msg broadcast" VSHIFT="23">
+<node CREATED="1377132581168" ID="ID_112861534" MODIFIED="1377133019628" TEXT="serverApp requests broadcast&#xa;kwez.org/androidPush/transferUpdt&#xa;payload=&quot;..&quot;"/>
+<node CREATED="1377132591823" ID="ID_146670919" MODIFIED="1377133504453" TEXT="marcoPolo broadcasts msg &#xa;to all registered clientApps"/>
+<node CREATED="1377133512054" ID="ID_297170486" MODIFIED="1377133519462" TEXT="(no retry / ack)"/>
+</node>
+<node CREATED="1377101715687" ID="ID_699904154" MODIFIED="1377130078247" POSITION="left" TEXT="messages">
+<node CREATED="1377130119924" FOLDED="true" ID="ID_835544906" MODIFIED="1377133815286" TEXT="&quot;m.polo:{JSON object msg}&quot;&#xa;marcoPolo msg/cmd">
+<node CREATED="1377130359609" FOLDED="true" HGAP="84" ID="ID_1497430216" MODIFIED="1377133675808" TEXT="version" VSHIFT="7">
+<node CREATED="1377130366619" ID="ID_1482147845" MODIFIED="1377130369433" TEXT="major"/>
+<node CREATED="1377130370145" ID="ID_1652153778" MODIFIED="1377130372436" TEXT="minor"/>
+</node>
+<node CREATED="1377132065856" FOLDED="true" HGAP="80" ID="ID_170084488" MODIFIED="1377133419015" TEXT="action" VSHIFT="46">
+<node CREATED="1377132657041" ID="ID_1212759630" MODIFIED="1377132671799" TEXT="register.app"/>
+<node CREATED="1377132661197" ID="ID_847473981" MODIFIED="1377132674698" TEXT="unregister.app"/>
+<node CREATED="1377132068450" ID="ID_1325827875" MODIFIED="1377132677085" TEXT="register.msg"/>
+<node CREATED="1377132113524" ID="ID_1571369487" MODIFIED="1377132679866" TEXT="unregister.msg"/>
+<node CREATED="1377132115928" ID="ID_1697067950" MODIFIED="1377132682716" TEXT="query.msg"/>
+<node CREATED="1377132235378" ID="ID_841304758" MODIFIED="1377133113642" TEXT="broadcast.msg"/>
+</node>
+<node CREATED="1377101722881" FOLDED="true" HGAP="47" ID="ID_1984794553" MODIFIED="1377133416224" TEXT="msg name" VSHIFT="26">
+<node CREATED="1377101733263" HGAP="22" ID="ID_1176621873" MODIFIED="1377101892602" TEXT="Domain&#xa;eg kwez.org" VSHIFT="5">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1377101754511" ID="ID_124424908" MODIFIED="1377101901955" TEXT="AppName&#xa;eg androidPush">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1377101767994" ID="ID_763250280" MODIFIED="1377101913513" TEXT="Method/msg etc&#xa;eg notifPushAvail">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1377101797478" HGAP="27" ID="ID_1615771619" MODIFIED="1377132873116" TEXT="&apos;kwez.org/androidPush/notifPushAvail&apos;" VSHIFT="19">
+<icon BUILTIN="info"/>
+</node>
+</node>
+<node CREATED="1377132835247" HGAP="55" ID="ID_1428439205" MODIFIED="1377133413741" TEXT="opt msg payload (string)&#xa;for broadcast msgs" VSHIFT="25"/>
+</node>
+<node CREATED="1377133135659" FOLDED="true" HGAP="42" ID="ID_1250123759" MODIFIED="1377133816898" TEXT="&quot;m.polo.bcast:{JSON obj}&quot;&#xa;broadcast msg" VSHIFT="58">
+<node CREATED="1377133150147" HGAP="55" ID="ID_730864761" MODIFIED="1377133363945" TEXT="msg name" VSHIFT="-7"/>
+<node CREATED="1377133341102" HGAP="52" ID="ID_996088479" MODIFIED="1377133361022" TEXT="msg payload (JSON string)" VSHIFT="21"/>
+</node>
+</node>
+</node>
+</map>
--- marcoPolo/marco polo.mm
+++ marcoPolo/marco polo.mm
@@ -2,7 +2,7 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1377099294490" ID="ID_394723472" MODIFIED="1377101709696" STYLE="fork" TEXT="marco polo">
 <font NAME="SansSerif" SIZE="15"/>
-<node CREATED="1377100373355" FOLDED="true" ID="ID_959423427" MODIFIED="1377101710618" POSITION="right" TEXT="marcoPolo / app connect">
+<node CREATED="1377100373355" ID="ID_959423427" MODIFIED="1377130608997" POSITION="right" TEXT="marcoPolo / app connect">
 <node CREATED="1377099309997" HGAP="23" ID="ID_1115893101" MODIFIED="1377100796904" STYLE="fork" TEXT="waits on UDP port for connections / requests" VSHIFT="-24">
 <font NAME="SansSerif" SIZE="15"/>
 <node CREATED="1377099957835" HGAP="26" ID="ID_781887355" MODIFIED="1377100410620" TEXT="could use range/list of ports&#xa;in case of failure to open port" VSHIFT="10">
@@ -17,16 +17,18 @@
 <icon BUILTIN="help"/>
 <node CREATED="1377100725872" HGAP="21" ID="ID_535245107" MODIFIED="1377101429665" TEXT="unregister app when no answer" VSHIFT="5"/>
 </node>
+<node CREATED="1377130641795" ID="ID_278539293" MODIFIED="1377130653556" TEXT="more dependable than UDP !"/>
 </node>
 </node>
 <node CREATED="1377101264226" ID="ID_852292473" MODIFIED="1377101447969" TEXT="When app closes:&#xa;&quot;unregister me&quot; (appId)">
-<node CREATED="1377101324067" HGAP="25" ID="ID_924797808" MODIFIED="1377101394464" TEXT="could be done by just closing permanant TCP socket" VSHIFT="12">
+<node CREATED="1377101324067" HGAP="25" ID="ID_924797808" MODIFIED="1377131047094" TEXT="could be done by just closing permanent TCP socket" VSHIFT="12">
 <icon BUILTIN="help"/>
 </node>
 </node>
 </node>
-<node CREATED="1377100598364" FOLDED="true" HGAP="41" ID="ID_1798721036" MODIFIED="1377101672205" POSITION="right" TEXT="broadcast msgs" VSHIFT="25">
-<node CREATED="1377100253654" ID="ID_1424147096" MODIFIED="1377101084745" TEXT="serverApp sends :&#xa;kwez.org/androidPush/notif&apos;" VSHIFT="5">
+<node CREATED="1377100598364" HGAP="41" ID="ID_1798721036" MODIFIED="1377130627216" POSITION="right" TEXT="broadcast msgs" VSHIFT="25">
+<node CREATED="1377130846820" ID="ID_1427078905" MODIFIED="1377131317906" TEXT="clientApp registers for msg&#xa;kwez.org/androidPush/notif"/>
+<node CREATED="1377100253654" ID="ID_1424147096" MODIFIED="1377131302475" TEXT="serverApp sends :&#xa;kwez.org/androidPush/notif" VSHIFT="5">
 <icon BUILTIN="forward"/>
 <node CREATED="1377101011489" HGAP="34" ID="ID_363667393" MODIFIED="1377101140964" TEXT="marcoPolo sends back answer to serverApp" VSHIFT="-6">
 <icon BUILTIN="back"/>
@@ -36,7 +38,7 @@
 <icon BUILTIN="forward"/>
 </node>
 </node>
-<node CREATED="1377099524331" ID="ID_1977219182" MODIFIED="1377101063660" STYLE="fork" TEXT="clientApp registered to:&#xa;&apos;kwez.org/androidPush/notif&apos;">
+<node CREATED="1377099524331" ID="ID_1977219182" MODIFIED="1377131310058" STYLE="fork" TEXT="clientApp registered to:&#xa;kwez.org/androidPush/notif">
 <font NAME="SansSerif" SIZE="15"/>
 <node CREATED="1377100984398" HGAP="49" ID="ID_1901858870" MODIFIED="1377101153884" TEXT="possible answer" VSHIFT="16">
 <arrowlink DESTINATION="ID_363667393" ENDARROW="Default" ENDINCLINATION="163;0;" ID="Arrow_ID_84087455" STARTARROW="None" STARTINCLINATION="163;0;"/>
@@ -45,17 +47,32 @@
 </node>
 </node>
 </node>
-<node CREATED="1377099674415" FOLDED="true" ID="ID_558032627" MODIFIED="1377102071480" POSITION="right" STYLE="fork" TEXT="&apos;services&apos; discovery" VSHIFT="31">
+<node CREATED="1377099674415" ID="ID_558032627" MODIFIED="1377130628808" POSITION="right" STYLE="fork" TEXT="&apos;services&apos; discovery" VSHIFT="31">
 <font NAME="SansSerif" SIZE="14"/>
-<node CREATED="1377101566981" ID="ID_58866484" MODIFIED="1377101952782" TEXT="appY registered as service&#xa;&apos;kwez.org/androidPush/getPush&apos;&#xa;(on connect)"/>
+<node CREATED="1377101566981" ID="ID_58866484" MODIFIED="1377131078270" TEXT="appY registers for service req&#xa;&apos;kwez.org/androidPush/getPush&apos;"/>
 <node CREATED="1377099684469" HGAP="24" ID="ID_448274881" MODIFIED="1377101666564" STYLE="fork" TEXT="appX queries for service&#xa;&apos;kwez.org/androidPush/getPush&apos;" VSHIFT="17">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 <node CREATED="1377101959898" ID="ID_1955642783" MODIFIED="1377102003030" TEXT="marcoPolo answers appX with&#xa;&apos;appY&apos; @ tcp:host:port&#xa;or @ udp:host:port"/>
 <node CREATED="1377102046425" ID="ID_1692447688" MODIFIED="1377102064457" TEXT="appX connects to appY ..."/>
 </node>
-<node CREATED="1377101715687" FOLDED="true" ID="ID_699904154" MODIFIED="1377101932822" POSITION="left" TEXT="messages">
-<node CREATED="1377101722881" HGAP="45" ID="ID_1984794553" MODIFIED="1377101929463" TEXT="method/msg&apos;address&apos;" VSHIFT="-27">
+<node CREATED="1377101715687" ID="ID_699904154" MODIFIED="1377130078247" POSITION="left" TEXT="messages">
+<node CREATED="1377130119924" ID="ID_835544906" MODIFIED="1377130588430" TEXT="&quot;m.polo:{object msg}&quot;">
+<node CREATED="1377130283900" ID="ID_1902909448" MODIFIED="1377130300208" TEXT="object is JSON"/>
+<node CREATED="1377130307048" ID="ID_762380221" MODIFIED="1377130358788" TEXT="std header ?">
+<node CREATED="1377130359609" ID="ID_1497430216" MODIFIED="1377130366342" TEXT="version">
+<node CREATED="1377130366619" ID="ID_1482147845" MODIFIED="1377130369433" TEXT="major"/>
+<node CREATED="1377130370145" ID="ID_1652153778" MODIFIED="1377130372436" TEXT="minor"/>
+</node>
+<node CREATED="1377130417039" ID="ID_1399529973" MODIFIED="1377130431714" TEXT="type">
+<node CREATED="1377130432366" ID="ID_1963572835" MODIFIED="1377130434779" TEXT="callback"/>
+<node CREATED="1377130435296" ID="ID_1244585182" MODIFIED="1377130447288" TEXT="callback+response"/>
+<node CREATED="1377130447967" ID="ID_117900058" MODIFIED="1377130478004" TEXT="service"/>
+</node>
+<node CREATED="1377130381324" ID="ID_1518618271" MODIFIED="1377130415398" TEXT="msgName"/>
+</node>
+</node>
+<node CREATED="1377101722881" FOLDED="true" HGAP="45" ID="ID_1984794553" MODIFIED="1377130086760" TEXT="method/msg&apos;address&apos;" VSHIFT="-27">
 <node CREATED="1377101733263" HGAP="22" ID="ID_1176621873" MODIFIED="1377101892602" TEXT="Domain&#xa;eg kwez.org" VSHIFT="5">
 <icon BUILTIN="full-1"/>
 </node>
