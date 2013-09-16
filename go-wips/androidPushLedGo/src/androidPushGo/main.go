8a64210135f051419c22bc28ad859a43b6a2fa9b
Philippe Quesnel <philippe.quesnel@gmail.com>
2013-09-15 23:42:49 -0400

    + moved iupgo lib to single standard GOPATH dir, + doing some tests with onIdle func

--- go-wips/androidPushLedGo/src/androidPushGo/main.go
+++ go-wips/androidPushLedGo/src/androidPushGo/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/grd/iup"
 	"iuputil"
+	"runtime"
 	"time"
 )
 
@@ -11,23 +12,92 @@ type MyWidgets struct {
 	MainDialog *iup.Ihandle `IUP:"mainDialog"`
 	LocalRoot  *iup.Ihandle `IUP:"localRoot"`
 	Files      *iup.Ihandle
+	Push       *iup.Ihandle `IUP:"pushButton"`
 }
 
-var idleChan chan string
+var cmdChan chan string
+var quitChan chan struct{}
 var cpt int
 var myWidgets MyWidgets
 
-func idleFunc() int {
+//---------
+
+func idleFunc1() int {
 	select {
-	case cmd := <-idleChan:
+	case cmd := <-cmdChan:
 		iup.SetAttribute(myWidgets.LocalRoot, "VALUE", cmd)
 		fmt.Println("got something to do in idle: ", cmd)
-	case <-time.After(time.Duration(150 * time.Millisecond)):
+
+	case <-time.After(time.Duration(50 * time.Millisecond)):
 	}
 	return iup.DEFAULT
 }
 
+//----------
+
+func idleFunc2() int {
+	var i = 0
+	for iup.LoopStepWait() != iup.CLOSE {
+		fmt.Println("iup.LoopStepWait", i)
+		i++
+
+		// this won't get called until an UI event occurs !
+		// on Linux/GTK we do get called (some timer event ?)
+		select {
+		case cmd := <-cmdChan:
+			iup.SetAttribute(myWidgets.LocalRoot, "VALUE", cmd)
+			fmt.Println("got something to do in idle: ", cmd)
+		default:
+		}
+	}
+
+	fmt.Println("idleFunc2 exit")
+	return iup.CLOSE
+}
+
+//-----
+
+// ## does not work !!
+// the GUI is frozen .. seems like iup.LoopStepWait() never returns
+
+func myIupLoop() {
+	runtime.LockOSThread()
+
+	var i = 0
+	for iup.LoopStepWait() != iup.CLOSE {
+		fmt.Println("iup.LoopStepWait", i)
+		i++
+	}
+
+	close(quitChan)
+}
+
+func idleFunc3() int {
+
+	//runtime.LockOSThread()
+
+	go myIupLoop()
+
+	for {
+		select {
+		case cmd := <-cmdChan:
+			iup.SetAttribute(myWidgets.LocalRoot, "VALUE", cmd)
+			fmt.Println("got something to do in idle: ", cmd)
+
+		case <-quitChan:
+			break
+		}
+	}
+
+	fmt.Println("idleFunc3 exit")
+	return iup.CLOSE
+}
+
+//----------
+
 func main() {
+	runtime.LockOSThread()
+
 	iup.Open()
 	defer iup.Close()
 
@@ -46,15 +116,16 @@ func main() {
 
 	// prepare a channel for the idle callback,
 	// start a goroutine to send a msg on the channel after some time
-	idleChan = make(chan string)
+	cmdChan = make(chan string)
+	quitChan = make(chan struct{})
 
 	go func() {
 		time.Sleep(time.Duration(2) * time.Second)
-		idleChan <- "command to process by UI thread"
+		cmdChan <- "command to process by UI thread"
 	}()
 
 	// hook our idle func
-	iup.SetIdleFunc(idleFunc)
+	iup.SetIdleFunc(idleFunc1)
 
 	/* main loop */
 	iup.MainLoop()
